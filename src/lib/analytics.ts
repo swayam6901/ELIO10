da8622134942ad06dfc94868d7eb7c9b28104f59
swayam6901 <swayam837@gmail.com>
2025-08-23 22:02:16 +0530

    Update analytics.ts

--- src/lib/analytics.ts
+++ src/lib/analytics.ts
@@ -1,1 +1,6 @@
-
+export const track = (event: string, props?: Record<string, any>) => {
+if (process.env.NODE_ENV === 'development') {
+// eslint-disable-next-line no-console
+console.log(`[track] ${event}`, props)
+}
+}
